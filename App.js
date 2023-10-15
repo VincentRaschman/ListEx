88a0c480ea1baef1c6d255863ad2c5926b0ed579
Vincent Raschman <vincent.raschman@gmail.com>
2023-10-15 21:41:50 +0200

    TestTest

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
+  const [text, changeText] = React.useState("");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <TextInput value={text} 
+      onChange={changeText}
+      placeholder='Write yourself an objetive' />
     </View>
   );
 }
